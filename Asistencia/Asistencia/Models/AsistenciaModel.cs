b6d4e63ef4024e9d84036fb53cc557218454fc21
Daniela <danipacheco29@gmail.com>
2018-06-14 22:20:06 -0300

    Proyecto Asistecias

---
+++ Asistencia/Asistencia/Models/AsistenciaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asistencia.Models
+{
+    public class AsistenciaModel
+    {
+        public string Nombre  {get;set;}
+        public string Apellido {get;set;}
+        public int Id {get;set;}
+        public DateTime Fecha {get;set;}
+
+
+    }
+}
